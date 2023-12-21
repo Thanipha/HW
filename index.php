33ec555e33164fc38fc63e2d07adcc590ceed424
Thanipha <65160325@go.buu.ac.th>
2023-12-21 11:11:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <html>
-
 <head>
     <meta charset="utf-8">
 </head>
-
 <body>
     <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
+    <div class = "HW">
     <table>
         <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
+            <?php
+            for($i = 1 ; $i <= 24 ; $i++){
+                echo "<tr>";
+                echo "<td> $multi_x X $i = ".$multi_x*$i."</td>";
+                echo "</tr>";
+            }
+            ?>
         </tbody>
     </table>
     <style>
         table {
             width: 150px;
-            height: 265px;
+            height: 200px;
+        }
+        div.HW{
+            width: 300px;
+            height: 200px;
+            overflow-y : scroll ;
         }
     </style>
 </body>
-
 </html>
